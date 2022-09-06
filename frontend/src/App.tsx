59c5018386522332b6f62d549e1e6905b7ca273f
Uillianleal <willianleal52@hotmail.com>
2022-09-06 15:27:32 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from './components/NotificationButton';
 
 function App() {
   return (
-    <h1>olá mundo</h1>
-  )
+    <>
+      <h1>olá mudo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
